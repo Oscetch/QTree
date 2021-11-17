2f751927e0fc47c8441fea2fcaaafc6d294bc9ad
Oscar <halldinoscar@gmail.com>
2021-11-17 17:29:11 +0100

    Added quadtree that grows in size. Also added a testing tool

---
+++ QTree.TestTool/Dialogs/SizeDialog.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace QTree.TestTool.Dialogs
+{
+    partial class SizeDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.widthNumeric = new System.Windows.Forms.NumericUpDown();
+            this.heightNumeric = new System.Windows.Forms.NumericUpDown();
+            this.okButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.widthNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Width:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Height:";
+            // 
+            // widthNumeric
+            // 
+            this.widthNumeric.Location = new System.Drawing.Point(75, 7);
+            this.widthNumeric.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.widthNumeric.Name = "widthNumeric";
+            this.widthNumeric.Size = new System.Drawing.Size(159, 23);
+            this.widthNumeric.TabIndex = 1;
+            this.widthNumeric.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // heightNumeric
+            // 
+            this.heightNumeric.Location = new System.Drawing.Point(75, 37);
+            this.heightNumeric.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.heightNumeric.Name = "heightNumeric";
+            this.heightNumeric.Size = new System.Drawing.Size(159, 23);
+            this.heightNumeric.TabIndex = 1;
+            this.heightNumeric.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(12, 79);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "Ok";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(93, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // SizeDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 114);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.heightNumeric);
+            this.Controls.Add(this.widthNumeric);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SizeDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "SizeDialog";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.widthNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown widthNumeric;
+        private System.Windows.Forms.NumericUpDown heightNumeric;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ QTree.TestTool/Dialogs/SizeDialog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QTree.TestTool.Dialogs
+{
+    public partial class SizeDialog : Form
+    {
+        public int SelectedWidth { get; private set; }
+        public int SelectedHeight { get; private set; }
+
+        public SizeDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            SelectedWidth = (int)widthNumeric.Value;
+            SelectedHeight = (int)heightNumeric.Value;
+
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
